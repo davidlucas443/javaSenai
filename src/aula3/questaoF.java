a37857bae7386cc41d5103aaccac71a952ccffb5
2DM <davidga19391@gmail.com>
2025-09-15 08:34:12 -0300

    feat: created aula3

---
+++ src/aula3/questaoF.java
@@ -0,0 +1,27 @@
+package aula3;
+
+import java.util.Scanner;
+
+public class questaoF {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+
+		Double a, b, c, d;
+		
+		System.out.println("digite o valor de a");
+		a = sc.nextDouble();
+		
+		System.out.println("digite o valor de b");
+		b = sc.nextDouble();
+		
+		c = a;
+		d = b;
+		
+		System.out.println("o valor de A é" + d);
+		
+		System.out.println("o valor de B é " + c);
+				
+	}
+}
